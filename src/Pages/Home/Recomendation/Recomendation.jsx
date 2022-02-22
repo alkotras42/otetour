6c9cf4d88beb780cd5ac66d134d94da09ae0ff44
alkotras42 <kirill.stogniy@mail.ru>
2022-02-22 03:46:46 +0300

    Сделал блок рекомендаций со слайдером

--- src/Pages/Home/Recomendation/Recomendation.jsx
+++ src/Pages/Home/Recomendation/Recomendation.jsx
@@ -1,22 +1,67 @@
-import React, { useState } from 'react'
-
+import React, { useState, useRef } from 'react'
 import cn from 'classnames'
 import { Button, Calendar, Card, Location } from '../../../Component'
-import { Carousel } from 'react-responsive-carousel'
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import styles from './Recomendation.module.css'
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
+import arrowPrev from './arrowPrev.svg'
+import arrowNext from './arrowNext.svg'
+import { Pagination, Navigation } from 'swiper'
+import 'swiper/css'
+import 'swiper/css/navigation'
 
 export const Recomendation = ({ className, ...props }) => {
 	const [active, setActive] = useState(1)
 
+	const prevRef = useRef(null)
+	const nextRef = useRef(null)
+
 	return (
 		<div className={cn(className, styles.recomendation)}>
 			<span className={styles.title}>Рекомендации для вас</span>
-			<Carousel infiniteLoop='true' centerMode='true' selectedItem='3' width='1200px'>
-				<Card />
-				<Card />
-				<Card />
-			</Carousel>
+			<div className={styles.carouselWrapper}>
+				<img src={arrowPrev} alt='' ref={prevRef} className={styles.arrowPrev} />
+				<img src={arrowNext} alt='' ref={nextRef} className={styles.arrowNext} />
+				<div className={styles.carousel}>
+					<Swiper
+						onInit={(swiper) => {
+							swiper.params.navigation.prevEl = prevRef.current
+							swiper.params.navigation.nextEl = nextRef.current
+							swiper.navigation.init()
+							swiper.navigation.update()
+						}}
+						slidesPerView={3}
+						spaceBetween={20}
+						loop={true}
+						modules={[Navigation]}
+						className={styles.swiper}
+					>
+						<SwiperSlide>
+							<Card />
+						</SwiperSlide>
+						<SwiperSlide>
+							<Card />
+						</SwiperSlide>
+						<SwiperSlide>
+							<Card />
+						</SwiperSlide>
+						<SwiperSlide>
+							<Card />
+						</SwiperSlide>
+						<SwiperSlide>
+							<Card />
+						</SwiperSlide>
+						<SwiperSlide>
+							<Card />
+						</SwiperSlide>
+						<SwiperSlide>
+							<Card />
+						</SwiperSlide>
+						<SwiperSlide>
+							<Card />
+						</SwiperSlide>
+					</Swiper>
+				</div>
+			</div>
 		</div>
 	)
 }
