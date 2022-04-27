486e9221959b62906ddcd676f5aa4df25be486c4
alkotras42 <kirill.stogniy@mail.ru>
2022-04-27 20:58:15 +0300

    Добавил возможность загружать изображения для тура и удалять их

--- src/Pages/Profiles/GuidProfiles/AddTour/Steps/ThirdStep.js
+++ src/Pages/Profiles/GuidProfiles/AddTour/Steps/ThirdStep.js
@@ -13,6 +13,8 @@ import { imageFilter } from '../../../../../Helpers/helpers'
 const ThirdStep = ({ className, control, register, formStep, setFormStep, trigger, setValue, ...props }) => {
 	const [days, setDays] = useState([{ modal: false, image: '' }])
 
+	const [tourImages, setTourImages] = useState({ modal: false, images: [] })
+
 	const [imageError, setImageError] = useState(null)
 
 	const { fields, append, remove } = useFieldArray({
@@ -35,18 +37,6 @@ const ThirdStep = ({ className, control, register, formStep, setFormStep, trigge
 		setDays((prev) => prev.filter((_, i) => i !== index)) // Убираем из стейта объект по индексу
 	}
 
-	const openModal = (index) => {
-		const d = [...days] // create the copy of state array
-		d[index] = { ...d[index], modal: true } //new value
-		setDays(d)
-	}
-
-	const closeModal = (index) => {
-		const d = [...days] // create the copy of state array
-		d[index] = { ...d[index], modal: false } //new value
-		setDays(d)
-	}
-
 	const nextStep = async (e) => {
 		e.preventDefault()
 		const result = await trigger(['description', 'days'], { shouldFocus: true })
@@ -56,13 +46,48 @@ const ThirdStep = ({ className, control, register, formStep, setFormStep, trigge
 		}
 	}
 
+	const handleTourImageDrop = (dropped) => {
+		const response = imageFilter(dropped[0])
+		if (response.ok) {
+			setImageError(null)
+			const img = dropped[0]
+			const reader = new FileReader()
+			reader.onload = (event) => {
+				// const d = [...days] // create the copy of state array
+				// d[index] = { modal: false, image: event.target.result } //new value
+				// setDays(d)
+				setTourImages({ modal: false, images: [...tourImages.images, event.target.result] })
+			}
+			reader.readAsDataURL(img)
+		} else {
+			setImageError(response.message)
+		}
+	}
+
+	const removeTourImage = (index) => {
+		setTourImages({ ...tourImages, images: [tourImages.images.filter((_, i) => i !== index)] }) // Убираем из стейта объект по индексу
+	}
+
 	const prevStep = (e) => {
 		e.preventDefault()
 		setFormStep((prev) => prev - 1)
 		document.documentElement.scrollTop = 0
 	}
 
-	const handleDrop = (dropped, index) => {
+	const openDayImageModal = (index) => {
+		const d = [...days] // create the copy of state array
+		d[index] = { ...d[index], modal: true } //new value
+		setDays(d)
+	}
+
+	const closeDayImageModal = (index) => {
+		setImageError(null)
+		const d = [...days] // create the copy of state array
+		d[index] = { ...d[index], modal: false } //new value
+		setDays(d)
+	}
+
+	const handleDayImageDrop = (dropped, index) => {
 		const response = imageFilter(dropped[0])
 		if (response.ok) {
 			setImageError(null)
@@ -77,21 +102,6 @@ const ThirdStep = ({ className, control, register, formStep, setFormStep, trigge
 		} else {
 			setImageError(response.message)
 		}
-		// if (dropped[0].size > 5e6) {
-		// 	setImageError('Изображение не должно превышать 5Мб')
-		// } else if (dropped[0].type.toString() !== ('image/jpeg' || 'image/png')) {
-		// 	setImageError('Неверный тип файла, выберите изображение формата png, jpg или jpeg')
-		// } else {
-		// 	setImageError(null)
-		// 	const img = dropped[0]
-		// 	const reader = new FileReader()
-		// 	reader.onload = (event) => {
-		// 		const d = [...days] // create the copy of state array
-		// 		d[index] = { modal: false, image: event.target.result } //new value
-		// 		setDays(d)
-		// 	}
-		// 	reader.readAsDataURL(img)
-		// }
 	}
 
 	return (
@@ -106,7 +116,41 @@ const ThirdStep = ({ className, control, register, formStep, setFormStep, trigge
 			/>
 			<p className={styles.blockTitle}>Фотографии</p>
 			<p>Добавьте до 10 изображений, показывающих основные впечатления тура.</p>
-			<img className={styles.addPhoto} src={PhotoIcon} alt='' />
+			<div className={styles.tourImages}>
+				<>
+					{tourImages.images.length !== 0 &&
+						tourImages.images.map((image, index) => (
+							<div key={index} className={styles.tourImageItem}>
+								<img src={CloseIcon} alt='' className={styles.tourImageCloseIcon} onClick={() => removeTourImage(index)} />
+								<img src={image} alt='' className={styles.tourImage} />
+							</div>
+						))}
+					<img
+						className={styles.addPhoto}
+						src={PhotoIcon}
+						alt=''
+						onClick={() => setTourImages({ ...tourImages, modal: true })}
+					/>
+				</>
+			</div>
+
+			<Modal
+				isOpen={tourImages.modal}
+				onRequestClose={() => setTourImages({ ...tourImages, modal: false })}
+				className={styles.modal}
+			>
+				{imageError && <p className={styles.error}>{imageError}</p>}
+				<Dropzone onDropAccepted={handleTourImageDrop}>
+					{({ getRootProps, getInputProps }) => (
+						<div {...getRootProps({ className: styles.dropzone })}>
+							<div>
+								<input {...getInputProps()} />
+								<p>Перетащите изобращение сюда или нажмите чтобы выбрать.</p>
+							</div>
+						</div>
+					)}
+				</Dropzone>
+			</Modal>
 			<p className={styles.blockTitle}>Программа по дням</p>
 			<p>Распишите подробную программу по дням, добавьте фотографии.</p>
 			{fields.map((field, index) => (
@@ -116,15 +160,15 @@ const ThirdStep = ({ className, control, register, formStep, setFormStep, trigge
 						{index > 0 && <img className={styles.closeIcon} src={CloseIcon} alt='' onClick={() => removeDay(index)} />}
 					</div>
 					<div className={styles.dayDescription}>
-						<img className={styles.addPhoto} src={PhotoIcon} onClick={() => openModal(index)} alt='' />
+						<img className={styles.addPhoto} src={PhotoIcon} onClick={() => openDayImageModal(index)} alt='' />
 						{days[index].image && <img className={styles.dayImage} src={days[index].image} alt='' />}
-						<Modal isOpen={days[index].modal} onRequestClose={() => closeModal(index)} className={styles.modal}>
+						<Modal isOpen={days[index].modal} onRequestClose={() => closeDayImageModal(index)} className={styles.modal}>
 							{imageError && <p className={styles.error}>{imageError}</p>}
-							<Dropzone onDropAccepted={(dropped) => handleDrop(dropped, index)}>
+							<Dropzone onDropAccepted={(dropped) => handleDayImageDrop(dropped, index)}>
 								{({ getRootProps, getInputProps }) => (
 									<div {...getRootProps({ className: styles.dropzone })}>
 										<div>
-											<input {...getInputProps()} name={index} />
+											<input {...getInputProps()} />
 											<p>Перетащите изобращение сюда или нажмите чтобы выбрать.</p>
 										</div>
 									</div>
